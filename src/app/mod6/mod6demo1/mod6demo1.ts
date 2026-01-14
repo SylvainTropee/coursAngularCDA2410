1bde68c13100009d975d6ed3045545b60c9941ab
stropee <stropee@campus-eni.fr>
2026-01-14 14:17:34 +0100

    Module 6 form

--- src/app/mod6/mod6demo1/mod6demo1.ts
+++ src/app/mod6/mod6demo1/mod6demo1.ts
@@ -1,10 +1,11 @@
 import {Component} from '@angular/core';
-import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
+import {AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-mod6demo1',
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    FormsModule
   ],
   templateUrl: './mod6demo1.html',
   styleUrl: './mod6demo1.css',
@@ -13,12 +14,13 @@ import {AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators
 export class Mod6demo1 {
 
   public form: FormGroup
+  public name? : string
 
   constructor() {
     this.form = new FormGroup(
       {
         email: new FormControl(null, [Validators.email, Validators.required]),
-        password: new FormControl(null, [Validators.required, Validators.pattern(/[a-z0-9]{8,}/gi)]),
+        password: new FormControl(null, [Validators.required, Validators.pattern(/[a-z0-9]{8,}/i)]),
         birthdate: new FormControl(null, [Validators.required, this.dateValidator]),
         newsLetter : new FormControl(true),
         job : new FormControl('dev'),
@@ -36,4 +38,20 @@ export class Mod6demo1 {
     return null;
   }
 
+  onSubmit(){
+    if(this.form.valid){
+      //si c'est ok je récupère les valeurs et j'envoie à un service
+      console.log(this.form.value)
+    }else{
+      console.log("C'est pas bon")
+    }
+  }
+
+
+  check(event : Event) {
+    console.log(event)
+    if(this.name?.trim().length == 0){
+      console.log("Champ requis !")
+    }
+  }
 }
